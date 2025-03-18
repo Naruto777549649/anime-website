8552a821432af98b243eaa6a27af1bb06dbe9a58
Naruto777549649 <sikrar026@gmail.com>
2025-03-18 14:23:51 +0530

    Update script.js

--- templates/static/script.js
+++ templates/static/script.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website Loaded Successfully!");
+// Dark Mode Toggle
+document.querySelector("header").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
 });
+
+// Anime API Se Data Fetch
+fetch('https://api.jikan.moe/v4/anime?q=naruto')
+  .then(response => response.json())
+  .then(data => console.log(data));
